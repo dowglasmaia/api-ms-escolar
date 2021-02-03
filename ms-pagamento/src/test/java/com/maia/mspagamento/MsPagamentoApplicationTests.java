ccdfb4fe92057b82b68aec2975291c2f3af7daf6
dowglasmaia <dowglasmaia@gmail.com>
2021-02-03 14:50:43 -0300

    criando projeto

---
+++ ms-pagamento/src/test/java/com/maia/mspagamento/MsPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maia.mspagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
