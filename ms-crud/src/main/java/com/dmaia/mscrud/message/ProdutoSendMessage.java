5e9fc0a80e5ed6f587747efff8f90563e0040ead
dowglasmaia <dowglasmaia@gmail.com>
2021-02-09 11:57:58 -0300

    criando conf para rabbitmq e conf docker-compose

---
+++ ms-crud/src/main/java/com/dmaia/mscrud/message/ProdutoSendMessage.java
@@ -0,0 +1,29 @@
+package com.dmaia.mscrud.message;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.dmaia.mscrud.entity.vo.ProdutoVO;
+
+@Component
+public class ProdutoSendMessage {
+
+	@Value("${crud.rabbitmq.exchange}")
+	protected String exchange;
+
+	@Value("${crud.rabbitmq.routingkey}")
+	protected String routingKey;
+
+	public final RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	public ProdutoSendMessage(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+	
+	public void sendMessage(ProdutoVO produtoVO) {
+		rabbitTemplate.convertAndSend(exchange, routingKey, produtoVO);
+	}
+}
