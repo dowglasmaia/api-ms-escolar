4ba03bba04ac2e0b32a31471bf00e97bd483efc5
dowglasmaia <dowglasmaia@gmail.com>
2021-02-18 14:30:11 -0300

    criando api-ms-zuul-gateway

---
+++ ms-zuul-gateway/src/main/java/org/maia/mszuulgateway/MsZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.maia.mszuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsZuulGatewayApplication.class, args);
+	}
+
+}
